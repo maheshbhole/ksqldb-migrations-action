1183cb643d35dce31f54f908580edb63a7aa7576
maheshbhole <75613900+maheshbhole@users.noreply.github.com>
2022-08-05 13:51:32 +0530

    test3

--- migrations/V000004__altertest.sql
+++ migrations/V000004__altertest.sql
@@ -1,13 +1,2 @@
-ALTER STREAM ALL_ORDER_EVENTS (
-  orderid STRING, 
-  name STRING, 
-  items ARRAY<STRING>,
-  cost DECIMAL(6, 2),
-  ordertime TIMESTAMP,
-  purchase_method STRING,
-  test STRING
-) WITH (
-  kafka_topic='all_order_events', 
-  partitions=1,
-  value_format='json'
-);
+ALTER STREAM ALL_ORDER_EVENTS
+  ADD test STRING;
